eba456024cb1ec59f44ad8934700ce4d795c6da2
kormonditamas2222 <t.kormond2006@gmail.com>
2025-01-17 12:19:46 +0100

    indit() elkeszítve, hibák kijavítva, main-ben két változó neve megváltoztatva

--- szamologep/Program.cs
+++ szamologep/Program.cs
@@ -20,24 +20,35 @@ namespace szamologep
 		{
 			return a * b;
 		}
-		static double osztas(int a, int b)
+		static double osztas(double a, double b)
 		{
 			return a / b;
 		}
-		static string szazalek(int a, int b)
+		static string szazalek(double a, double b)
 		{
 			return Convert.ToString(a / b * 100) + "%";
 		}
-
+		static void indit(int a, int b, char muveleti_jel)
+		{
+			switch (muveleti_jel)
+			{
+				case '+': Console.WriteLine("A két szám összege: " + osszeadas(a, b)); break;
+				case '-': Console.WriteLine("A két szám különbsége: " + kivonas(a, b)); break;
+				case '*': Console.WriteLine("A két szám szorzata: " + szorzas(a, b)); break;
+				case '/': Console.WriteLine("A két szám hányadosa: " + osztas(a, b)); break;
+				case '%': Console.WriteLine("A két szám százaléka: " + szazalek(a, b)); break;
+                default: break;
+			}
+		}
 		static void Main(string[] args)
 		{
 			Console.Write("Kérem adja meg az első számot: ");
-			int a = Convert.ToInt32(Console.ReadLine());
+			int elso = Convert.ToInt32(Console.ReadLine());
 			Console.Write("Kérem adja meg a második számot: ");
-			int b = Convert.ToInt32(Console.ReadLine());
+			int masodik = Convert.ToInt32(Console.ReadLine());
 			Console.Write("Az elvégezendő művelet jele: ");
 			char jel = Convert.ToChar(Console.ReadLine());
-			indit(a, b, jel);
+			indit(elso, masodik, jel);
 		}
 	}
 }
